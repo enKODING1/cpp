335624e4b9d92b7de458421b067f14c7d00c0c49
enkoding1 <ksjdevelop53@gmail.com>
2025-08-13 18:56:33 +0900

    ex05

---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,34 @@
+#include "Harl.hpp"
+#include <cstdlib>
+
+int main(void)
+{
+    Harl	harl;
+
+	std::cout << "Printing all harl could say." << std::endl << std::endl;
+	harl.complain("ERROR");
+	std::cout << std::endl;
+	harl.complain("WARNING");
+	std::cout << std::endl;
+	harl.complain("INFO");
+	std::cout << std::endl;
+	harl.complain("DEBUG");
+	std::cout << std::endl;
+	harl.complain("something else");
+	std::cout << std::endl;
+	std::cout << "Now you have 5 chances to make harl say something." << std::endl;
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << "input: ";
+		std::string input;
+		getline(std::cin, input);
+		if (std::cin.eof() == true)
+		{
+			std::cerr << "^D" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		harl.complain(input);
+		std::cout << std::endl;
+	}
+	return 0;
+}
