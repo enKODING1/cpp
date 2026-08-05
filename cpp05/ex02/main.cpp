c7580eb7aca0c576475020545aa57065b88b8da0
Seongjin Kang <65354945+enKODING1@users.noreply.github.com>
2026-08-06 01:29:09 +0900

    chore(05/ex03): makeForm이 동작하는지 확인

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -1,30 +1,26 @@
-#include "Bureaucrat.hpp"
-#include "./form/RobotomyRequestForm.hpp"
 #include "./form/PresidentialPardonForm.hpp"
+#include "./form/RobotomyRequestForm.hpp"
 #include "./form/ShrubberyCreationForm.hpp"
+#include "Bureaucrat.hpp"
 #include <iostream>
 
-int main(void)
-{
-    try
-    {
-        Bureaucrat b = Bureaucrat("skang", 0);
-        RobotomyRequestForm r = RobotomyRequestForm("skang1");
-        PresidentialPardonForm p = PresidentialPardonForm("skang2");
-        ShrubberyCreationForm s = ShrubberyCreationForm("skang3");
+int main(void) {
+  try {
+    Bureaucrat b = Bureaucrat("skang", 0);
+    RobotomyRequestForm r = RobotomyRequestForm("skang1");
+    PresidentialPardonForm p = PresidentialPardonForm("skang2");
+    ShrubberyCreationForm s = ShrubberyCreationForm("skang3");
 
-        // b.signForm(r);
-        // b.signForm(p);
-        b.signForm(s);
+    // b.signForm(r);
+    // b.signForm(p);
+    b.signForm(s);
 
-        // r.execute(b);
-        // p.execute(b);
-        b.executeForm(s);
-    }
-    catch (std::exception &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
+    // r.execute(b);
+    // p.execute(b);
+    b.executeForm(s);
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
 
-    return 0;
+  return 0;
 }
